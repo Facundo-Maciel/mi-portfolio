e5900f55f24ae36716424410715ed27fbe538aed
facundo-maciel <maci.facundo@gmail.com>
2025-03-25 20:55:49 -0300

    mi portfolio

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import './style.css'
+
+    const h2elements = document.getElementsByTagName('h2');
+    const h2 = h2elements[0];
+    console.dir(h2)
+
+    // selecciona por clase
+    // const pPorclase = document.querySelectorAll('.contenedor-tarjeta');
+    // console.log(pPorclase)
+
+/*     selecciona por id
+    const tituloNav = document.getElementById('nav')
+    console.log(tituloNav) */
